aabd0ec6944495d84e8cee6f4733028d7564607a
haquesomrat <ihsomrat.official@gmail.com>
2024-02-01 17:47:48 +0600

    added brands

--- scripts/owl-carousel.js
+++ scripts/owl-carousel.js
@@ -19,3 +19,33 @@ $(".brands").owlCarousel({
     },
   },
 });
+
+// prodcuts
+
+$(".products").owlCarousel({
+  loop: true,
+  margin: 20,
+  dots: false,
+  nav: true,
+  navText: [
+    "<i class='fas fa-chevron-left'></i>",
+    "<i class='fas fa-chevron-right'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    992: {
+      items: 4,
+    },
+    1440: {
+      items: 6,
+    },
+  },
+});
