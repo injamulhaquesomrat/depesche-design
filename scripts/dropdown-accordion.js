ef579e4e417798d229e8d3b26316aca2d409be2b
haquesomrat <ihsomrat.official@gmail.com>
2024-02-13 19:36:16 +0600

    fixed some background color related bugs

--- scripts/dropdown-accordion.js
+++ scripts/dropdown-accordion.js
@@ -1,34 +1,54 @@
+// navbar accordion
 $(document).ready(function () {
   $(".accordionBtn").on("click", function () {
-    $(this).toggleClass("active");
+    // Remove the active class from all accordion buttons
+    $(".accordionBtn").not(this).removeClass("active");
 
+    // Hide the panels of all accordion items except the one that was clicked
+    $(".accordionBtn").not(this).next().css("display", "none");
+
+    // Reset the chevron rotation of all accordion items except the one that was clicked
+    $(".accordionBtn").not(this).children().css("transform", "rotate(0deg)");
+
+    // Toggle the active class and display of the clicked accordion item
+    $(this).toggleClass("active");
     let panel = $(this).next();
     let chevron = $(this).children();
 
     if (panel.css("display") === "block") {
       panel.css("display", "none");
-      chevron.css("transform", "rotate(0deg");
+      chevron.css("transform", "rotate(0deg)");
     } else {
       panel.css("display", "block");
-      chevron.css("transform", "rotate(180deg");
+      chevron.css("transform", "rotate(180deg)");
     }
   });
 });
 
 // filter accodion
+
 $(document).ready(function () {
   $(".filterBtn").on("click", function () {
-    $(this).toggleClass("active");
+    // Remove the active class from all accordion buttons
+    $(".filterBtn").not(this).removeClass("active");
 
+    // Hide the panels of all accordion items except the one that was clicked
+    $(".filterBtn").not(this).next().css("display", "none");
+
+    // Reset the chevron rotation of all accordion items except the one that was clicked
+    $(".filterBtn").not(this).children().css("transform", "rotate(0deg)");
+
+    // Toggle the active class and display of the clicked accordion item
+    $(this).toggleClass("active");
     let panel = $(this).next();
     let chevron = $(this).children();
 
     if (panel.css("display") === "block") {
       panel.css("display", "none");
-      chevron.css("transform", "rotate(0deg");
+      chevron.css("transform", "rotate(0deg)");
     } else {
       panel.css("display", "block");
-      chevron.css("transform", "rotate(180deg");
+      chevron.css("transform", "rotate(180deg)");
     }
   });
 });
